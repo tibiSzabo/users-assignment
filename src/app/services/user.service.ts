2a3f45625158070644b1e4c5a57f1d918ab6383a
Tibi <bukkfabiszti@gmail.com>
2020-12-23 12:33:44 +0000

    created skeleton of user service

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,10 +1,36 @@
 import { Injectable } from '@angular/core';
+import { User } from '../models/user.model';
+import { ServiceResponse } from '../models/service-response.model';
 
 @Injectable({
     providedIn: 'root'
 })
 export class UserService {
 
+    private _users: Array<User> = new Array<User>();
+    get users(): Array<User> {
+        return this._users;
+    }
+    set users(value: Array<User>) {
+        this._users = value;
+    }
+
     constructor() {
+    }
+
+    public getAllUsers(): ServiceResponse {
+        return null;
+    }
+
+    public addUser(user: User): ServiceResponse  {
+        return null;
+    }
+
+    public removeUser(id: number): ServiceResponse  {
+        return null;
+    }
+
+    public updateUser(user: User): ServiceResponse  {
+        return null;
     }
 }
