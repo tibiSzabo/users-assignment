953a9c94cc586c03972810b0b52cbd8ac9c172e1
Tibi <bukkfabiszti@gmail.com>
2020-12-23 12:25:55 +0000

    added some fields to user from https://jsonplaceholder.typicode.com/users api

--- src/app/models/user.model.ts
+++ src/app/models/user.model.ts
@@ -2,6 +2,10 @@ import { Deserializable } from './deserializable.model';
 
 export class User implements Deserializable {
 
+    id: number;
+    name: string;
+    username: string;
+    email: string;
 
     deserialize(input: any): this {
         Object.assign(this, input);
